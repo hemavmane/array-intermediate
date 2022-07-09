494c2ef829c909344da946096a515ccbe7b5e5aa
hemamane <hemamane5376@gmail.com>
2022-07-09 20:06:28 +0530

    array

---
+++ array.js
@@ -0,0 +1,40 @@
+// function isDuplicate(arr){
+//     let count=0;
+//  for(let i=0;i<arr.length;i++){
+  
+//  if(arr.lastIndexOf(arr[i])!=arr.indexOf(arr[i])){
+ 
+//    count++
+//  }
+//  }
+  
+//    if(count>0){
+//      return true
+//    }else{
+//     return false
+//    }
+//  }
+//  console.log(isDuplicate([1,2,3,4,5,5]))
+//  exports.isDuplicate = isDuplicate
+
+
+// function lostPlanet(planets){
+  
+//     let sorted=planets.sort((a,b)=>a-b)
+//      // console.log(sorted)
+//         let minNum = Math.min.apply(Math,sorted)
+//      // console.log(minNum)
+//         let maxNum = Math.max.apply(Math,sorted)
+//      // console.log(maxNum)
+//        for(let i=minNum;i<maxNum;i++){
+//         if(sorted.indexOf(i)<0){
+//               return i
+//         }
+//        }
+//                     }
+//    console.log(lostPlanet([1, 5, 7, 8, 3, 0, 2, 6]))
+//    exports.lostPlanet = lostPlanet
+   
+   
+       
+   
